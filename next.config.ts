1d11046b57cc563e91f877eefcc269a890d173ec
Adam Jareh <adamjareh.dev@gmail.com>
2025-03-05 14:58:15 +0000

    reinstate build checks

--- next.config.ts
+++ next.config.ts
@@ -14,12 +14,6 @@ const nextConfig: NextConfig = {
       },
     ],
   },
-  eslint: {
-    ignoreDuringBuilds: true, // TODO: remove. Temporarily skip checks
-  },
-  typescript: {
-    ignoreBuildErrors: true, // TODO: remove. Temporarily skip checks
-  },
 };
 
 export default withPayload(nextConfig);
